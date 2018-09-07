e9d49bf71f3067826486e11536ad560c0cea0ba6
Fabio Mugarte <fmcarvalho@stefanini.com>
2018-09-07 18:35:02 -0300

    Service worker funcionando

---
+++ sw-precache-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  navigateFallback : '/index.html',
+  stripPrefix: 'dist',
+  root:'dist/',
+  staticFileGlobs: [
+    'dist/index.html',
+    'dist/**.js',
+    'dist/**.css',
+    'dist/assets/D_MEGA.HTM'
+  ]
+}
+
+// module.exports = {
+//   navigateFallback : '/index.html',
+//   stripPrefix: 'dist',
+//   root:'dist/',
+//   staticFileGlobs: [
+//     'dist/index.html',
+//     'dist/**.js',
+//     'dist/**.css',
+//     'dist/assets/D_MEGA.HTM'
+//   ]
+// }
